0c3bde9f8672e3b0dc6fbb5601bd621fcbf2200e
ediop3SquadALT <205415956+ediop3SquadALT@users.noreply.github.com>
2025-06-22 01:39:29 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt install -y python3 python3-pip bluetooth bluez bluez-tools rfkill git
-pip3 install pydbus
+sudo apt install -y python3 python3-pip bluetooth bluez bluez-tools rfkill git python3-dbus
+pip3 install pexpect pydbus
 sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
 rfkill unblock bluetooth
